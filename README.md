4cf8d05e934cfddda135019b320de81afab08359
Christian O'Leary <christian.oleary@mtu.ie>
2024-07-18 11:03:54 +0100

    basic formatting

--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 Irish weather data from Met Eireann (Creative Commons Attribution 4.0 International (CC BY 4.0) License)
 
 <!-- https://cli.fusio.net/cli/climate_data/stations.csv -->
+
+## Development
+
+Installation:
+
+```bash
+conda create -n env_weather -y python=3.10
+conda activate env_weather
+pip install -r requirements.txt
+```
--- src/fetch_data.py
+++ src/fetch_data.py
@@ -1,19 +1,5 @@
 """Fetch public weather data for Ireland"""
 
-
-# declare -a levels=("h" "d" "m")
-# for i in {1000..9999}; do
-#     for l in "${levels[@]}"; do
-#         if [ -e $l'ly'$i'.zip' ]; then
-#             echo $l'ly'$i'.zip already exists'
-#         else
-#             echo 'Attemping download of '$l'ly'$i'.zip'
-#             curl -s -f 'https://cli.fusio.net/cli/climate_data/webdata/'$l'ly'$i'.zip' --output $l'ly'$i'.zip'
-#             sleep 5  # Just in case of rate limiting/automatic blacklisting
-#         fi
-#     done
-# done
-
 from pathlib import Path
 
 import pandas as pd
@@ -23,13 +9,18 @@ STATION_DATA_URL = 'https://cli.fusio.net/cli/climate_data/stations.csv'
 
 
 def fetch_data(
-    data_dir: str | Path = 'data',
-    station_url: str = 'https://cli.fusio.net/cli/climate_data/stations.csv',
+    data_dir: str | Path,
+    station_url: str,
 ):
+    """Fetch Met Eireann weather data and save to CSV files
+
+    :param str | Path data_dir: _description_, defaults to 'data'
+    :param _type_ station_url: _description_, defaults to 'https://cli.fusio.net/cli/climate_data/stations.csv'
+    """
 
     # Read station data to retrieve IDs
-    # df_stations = pd.read_csv(station_url)
-    # df_stations.to_csv(Path(data_dir, 'stations.csv'), index=False)
+    df_stations = pd.read_csv(station_url)
+    df_stations.to_csv(Path(data_dir, 'stations.csv'), index=False)
 
     df_stations = pd.read_csv(Path(data_dir, 'stations.csv'))
 
@@ -47,6 +38,20 @@ def fetch_data(
     for station_id, name in station_ids.items():
         print(station_id, name)
 
+    # TODO:
+    # declare -a levels=("h" "d" "m")
+    # for i in {1000..9999}; do
+    #     for l in "${levels[@]}"; do
+    #         if [ -e $l'ly'$i'.zip' ]; then
+    #             echo $l'ly'$i'.zip already exists'
+    #         else
+    #             echo 'Attemping download of '$l'ly'$i'.zip'
+    #             curl -s -f 'https://cli.fusio.net/cli/climate_data/webdata/'$l'ly'$i'.zip' --output $l'ly'$i'.zip'
+    #             sleep 5  # Just in case of rate limiting/automatic blacklisting
+    #         fi
+    #     done
+    # done
+
 
 if __name__ == '__main__':
-    fetch_data()
+    fetch_data(DATA_DIR, STATION_DATA_URL)
